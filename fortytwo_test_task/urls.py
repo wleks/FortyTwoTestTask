4c5ed69d3fb99b95f83120f112a21dc51f2594de
leksw <aleks.woronow@yandex.ru>
2015-08-31 10:38:21 +0000

    ticket:1 added static

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,3 +14,7 @@ urlpatterns = patterns(
     url(r'^', include('hello.urls', namespace='contact')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
